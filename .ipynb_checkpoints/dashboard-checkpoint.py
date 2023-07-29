d7412aca9a44aff0e8039b0d0f7dbdc15592a3bb
redtea3930 <red.tea3930@fastmail.com>
2023-07-28 14:25:20 -1000

    completed draft of dashboard, beginning readme, added Output file to repo directory

--- .ipynb_checkpoints/dashboard-checkpoint.py
+++ .ipynb_checkpoints/dashboard-checkpoint.py
@@ -58,30 +58,56 @@ def average_sales_price():
                    )
     return fig
 
+def average_rent_and_price():
+    """Average Gross Rent and Sale Price on same plot"""
+    # create subplot and right y axis objects with subplots() and twinx()
+    fig,ax = plt.subplots()
+    ax2=ax.twinx()
+    
+    # set labels for y axes
+    ax.set_ylabel('Sale Price per SqFt', color='blue', fontsize = 14)
+    ax2.set_ylabel('Gross Rent', color='red', fontsize = 14)
+    
+    # Sale price left y axis subplot with x axis label and title for full plot
+    price_plot = sfo_data['sale_price_sqr_foot'].groupby('year').mean()
+    price_plot.plot(title='Sales Price (blue, left) and Gross Rent (red, right) by Year',
+                    xlabel='Year',
+                    color="blue", 
+                    marker="o",
+                    ax=ax
+                   )
+    # Gross Rent right y axis subplot
+    rent_plot = sfo_data['gross_rent'].groupby('year').mean()
+    rent_plot.plot(color='red', 
+                   marker='o',
+                   ax=ax2)
+    
+    return fig
+    
 def average_price_by_neighborhood():
     """Average Prices by Neighborhood."""
     sfo_grouped = sfo_data.groupby(['year', 'neighborhood']).mean().reset_index()
     fig = sfo_grouped.hvplot.line(x='year',
-                        xlabel='Year', 
-                        y='sale_price_sqr_foot', 
-                        ylabel='Mean Sale Price per Sq Ft',
-                        groupby='neighborhood'
-                       )
+                                  xlabel='Year', 
+                                  y='sale_price_sqr_foot', 
+                                  ylabel='Mean Sale Price per Sq Ft',
+                                  groupby='neighborhood'
+                                 )
     return hv.render(fig)
 
 def top_most_expensive_neighborhoods():
     """Top 10 Most Expensive Neighborhoods."""
     top_10 = sfo_data.groupby('neighborhood').mean().nlargest(10, 'sale_price_sqr_foot').reset_index()
     fig = top_10.hvplot.bar(x='neighborhood',
-                  xlabel='Neighborhood',
-                  rot=90,
-                  y='sale_price_sqr_foot',
-                  ylabel='Mean Sale Price per Sq Ft,',
-                  title='Top 10 Most Expensive San Francisco Neighborhoods, Avg 2010-2016',
-                  frame_width=600,
-                  frame_height=250
-                 )
-    return hv.redner(fig)
+                            xlabel='Neighborhood',
+                            rot=37,
+                            y='sale_price_sqr_foot',
+                            ylabel='Mean Sale Price per Sq Ft,',
+                            title='Top 10 Most Expensive San Francisco Neighborhoods, Avg 2010-2016',
+                            frame_width=600,
+                            frame_height=250
+                           )
+    return hv.render(fig)
 
 def most_expensive_neighborhoods_rent_sales():
     """Comparison of Rent and Sales Prices of Most Expensive Neighborhoods."""
@@ -89,60 +115,76 @@ def most_expensive_neighborhoods_rent_sales():
     top_10_full =  sfo_data[sfo_data['neighborhood'].isin(top_10.neighborhood)]
     top_10_full_1 = top_10_full.drop(columns='housing_units').rename(columns={'sale_price_sqr_foot' : 'Mean Sales Price per Sq Ft', 'gross_rent' : 'Gross Rent' })
     fig = top_10_full_1.hvplot.bar(x='year', 
-                       rot=90, 
-                       groupby='neighborhood',
-                       height=500,
-                       title='Sale Price and Rent of 10 Most Expensive Neighborhoods, yearly 2010-2016' 
-                      )
+                                   rot=90, 
+                                   groupby='neighborhood',
+                                   height=500,
+                                   title='Sale Price and Rent of 10 Most Expensive Neighborhoods, yearly 2010-2016'
+                                  )
     return hv.render(fig)
     
 def parallel_coordinates():
     """Parallel Coordinates Plot."""
+    top_10 = sfo_data.groupby('neighborhood').mean().nlargest(10, 'sale_price_sqr_foot').reset_index()
     top_10['gross_rent'] = top_10['gross_rent'].round(2)
     top_10['sale_price_sqr_foot'] = top_10['sale_price_sqr_foot'].round(2)
     fig = px.parallel_coordinates(top_10,
-                       dimensions=['sale_price_sqr_foot', 'housing_units', 'gross_rent'],
-                       color='sale_price_sqr_foot',
-                       color_continuous_scale=px.colors.sequential.Inferno,
-                       labels={"sale_price_sqr_foot": "Sale Price per Sq Ft",
-                               "housing_units": "Housing Units",
-                               "gross_rent": 'Gross Rent'},)
+                                  title='Parallel Coordinates',
+                                  dimensions=['sale_price_sqr_foot', 
+                                              'housing_units', 
+                                              'gross_rent'
+                                             ],
+                                  color='sale_price_sqr_foot',
+                                  color_continuous_scale=px.colors.sequential.Inferno,
+                                  labels={"sale_price_sqr_foot": "Sale Price per Sq Ft",
+                                          "housing_units": "Housing Units",
+                                          "gross_rent": 'Gross Rent'
+                                         },
+                                 )
+    return fig
 
 def parallel_categories():
     """Parallel Categories Plot."""
+    top_10 = sfo_data.groupby('neighborhood').mean().nlargest(10, 'sale_price_sqr_foot').reset_index()
     top_10['gross_rent'] = top_10['gross_rent'].round(2)
     top_10['sale_price_sqr_foot'] = top_10['sale_price_sqr_foot'].round(2)
-    px.parallel_categories(top_10,
-                       dimensions=['neighborhood', 'sale_price_sqr_foot', 'housing_units', 'gross_rent'],
-                       color='sale_price_sqr_foot',
-                       color_continuous_scale=px.colors.sequential.Inferno,
-                       labels={"neighborhood": "Neighborhood",
-                               "sale_price_sqr_foot": "Sale Price per Sq Ft",
-                               "housing_units": "Housing Units",
-                               "gross_rent": 'Gross Rent'}
-                          )
+    fig = px.parallel_categories(top_10,
+                                 title='Parallel Categories',
+                                 dimensions=['neighborhood',
+                                             'sale_price_sqr_foot',
+                                             'housing_units',
+                                             'gross_rent'
+                                            ],
+                                 color='sale_price_sqr_foot',
+                                 color_continuous_scale=px.colors.sequential.Inferno,
+                                 labels={"neighborhood": "Neighborhood",
+                                         "sale_price_sqr_foot": "Sale Price per Sq Ft",
+                                         "housing_units": "Housing Units",
+                                         "gross_rent": 'Gross Rent'
+                                        }
+                                )
+    return fig
                            
 def neighborhood_map():
     """Neighborhood Map."""
     sfon = sfo_data.groupby('neighborhood').mean()
     sfo_location = pd.concat([neighborhood_data, sfon], axis=1)
     sfo_location = sfo_location.dropna()
-    map_plot = px.scatter_mapbox(
-        sfo_location,
-        title='Mean Sale Price (marker size) and Gross Rent (marker color) in San Francisco',
-        lat="Lat",
-        lon="Lon",
-        hover_name=sfo_location.index,
-        #Coordinates removed from hover data, end users unlikely to want precise lat/lon
-        hover_data={'Lat': False,
-                    'Lon': False,
-                   },
-        size='sale_price_sqr_foot',
-        size_max=15,
-        color='gross_rent',
-        color_continuous_scale='rainbow',
-        zoom=10
-    )
+    map_plot = px.scatter_mapbox(sfo_location,
+                                 title='Mean Sale Price (marker size) and Gross Rent (marker color) in San Francisco 2010-2016',
+                                 lat="Lat",
+                                 lon="Lon",
+                                 hover_name=sfo_location.index,
+                                 #Coordinates removed from hover data, end users unlikely to want precise lat/lon
+                                 hover_data={'Lat': False,
+                                             'Lon': False,
+                                            },
+                                 size='sale_price_sqr_foot',
+                                 size_max=15,
+                                 color='gross_rent',
+                                 color_continuous_scale='rainbow',
+                                 zoom=10
+                                )
+    return map_plot
 
 def sunburst():
     """Sunburst Plot."""
@@ -152,31 +194,50 @@ def sunburst():
                       path=[top_10_full.index, 'neighborhood'],
                       color='gross_rent',
                       color_continuous_scale='blues',
-                 )
+                     )
     return fig
 
 # Start Streamlit App
 st.header('San Francisco Rental Analysis Dashboard')
 
-# pandas plot streamlit call
-st.pyplot(housing_units_per_year())
-
-# hvplot plot streamlit call
-st.bokeh_chart(most_expensive_neighborhoods_rent_sales())
-
-# plotly plot Streamlit call
-st.plotly_chart(sunburst())
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-   st.header("MXE")
-   
-
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
+# Sidebar with selectbox to choose which plot to show
+with st.sidebar:
+    plot_choice = st.selectbox("Choose a plot:",
+                               ("Housing Units per Year", 
+                                'Average Gross Rent and Sale Price (seperate)',
+                                "Average Gross Rent and Sale Price (together)",
+                                "Average Price by Neighborhood",
+                                "Top 10 Expensive Neighborhoods",
+                                "Top Expensive Neighborhoods Rent and Sales",
+                                "Parallel Coordinates",
+                                "Parallel Categories",
+                                "Sunburst: Most expensive Neighborhoods 2010-2016",
+                                "Neighborhood Map: Rent and Sale Prices 2010-2016"
+                               )
+                              )
+    
+if (plot_choice == 'Housing Units per Year'):
+    st.pyplot(housing_units_per_year())
+elif (plot_choice == "Average Gross Rent and Sale Price (seperate)"):
+    col1, col2 = st.columns(2)
+    with col1:
+        st.pyplot(average_gross_rent())
+    with col2:
+        st.pyplot(average_sales_price())
+elif (plot_choice == "Average Gross Rent and Sale Price (together)"):
+    st.pyplot(average_rent_and_price())
+elif (plot_choice == "Average Price by Neighborhood"):
+    st.bokeh_chart(average_price_by_neighborhood())
+elif (plot_choice == "Top 10 Expensive Neighborhoods"):
+    st.bokeh_chart(top_most_expensive_neighborhoods())
+elif (plot_choice == "Top Expensive Neighborhoods Rent and Sales"):
+    st.bokeh_chart(most_expensive_neighborhoods_rent_sales())
+elif (plot_choice == "Parallel Coordinates"):
+    st.plotly_chart(parallel_coordinates())
+elif (plot_choice == "Parallel Categories"):
+    st.plotly_chart(parallel_categories())
+elif (plot_choice == "Sunburst: Most expensive Neighborhoods 2010-2016"):
+    st.plotly_chart(sunburst())
+elif (plot_choice == "Neighborhood Map: Rent and Sale Prices 2010-2016"):
+    st.plotly_chart(neighborhood_map())
+    
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# real-estate-dashboard
+# San Francisco Real Estate Dashboard
+
+This project takes a look at real estate data from San Francisco 2010-2016, focusing on sale prices and rental rates, with the idea of presenting what properties would potentially be worth buying to rent out.
+
+## Repository guide
+Starting Data (in [Data](https://github.com/redtea3930/real-estate-dashboard/tree/main/Data) folder):
+* [neighborhoods_coordinates.csv](https://github.com/redtea3930/real-estate-dashboard/blob/main/Data/neighborhoods_coordinates.csv) a small dataset of lat/long coordinates for named neighborhoods
+* [sfo_neighborhoods_census_data.csv](https://github.com/redtea3930/real-estate-dashboard/blob/main/Data/sfo_neighborhoods_census_data.csv) dataset of sales prices, number of housing units, and gross rent for named neighborhoods, sorted by year and neighborhood. Number of housing units and gross rent values are averages for each neighborhood by year.
+
+Output Data (in [Output]() folder):
+* [yearly_avg-units.csv](https://github.com/redtea3930/real-estate-dashboard/blob/main/yearly_avg_units.csv) a small dataframe of average total housing units across all named neighborhoods by year. Project instructions were to create this file in the course of the project
+* [concatenated.csv a concatenation of the inpu neighborhoods_coordinates.csv file with a processed version of the input sfo_neighborhoods_census_data.csv file. This file was created for visual analysis to determine data cleaning strategies while making the neighborhood map plot (see below)
+
+Code Files:
+* rental_analysis.ipynb a Python notebook with project instructions and initial coding, detailed in "Coding" section.
+* dashboard.py a Python script with finalized versions of plot coding and Streamlit dashboard code, detailed in "Dashboard" section.
+
+## Coding
+
+## Dashboard
